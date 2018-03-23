9dcc26e5c860f5fdbe3c4a74a97f9ba79e919c15
omateusp <37383185+omateusp@users.noreply.github.com>
2018-03-23 19:50:13 -0300

    Removed-almost-all-pointers

--- main.c
+++ main.c
@@ -43,80 +43,72 @@ void readImage(Image * img){
     }
 }
 
-void grayscale(Image * img) {
+Image grayscale(Image img) {
     // Transforming each pixel in grayscale by keeping the
     // three colors (r,g,b) and making an average of it
-    for (unsigned int i = 0; i < img->height; ++i) {
-        for (unsigned int j = 0; j < img->width; ++j) {
-            int average = img->pixel[i][j].r +
-                          img->pixel[i][j].g +
-                          img->pixel[i][j].b;
-            average /= 3;
-            img->pixel[i][j].r = average;
-            img->pixel[i][j].g = average;
-            img->pixel[i][j].b = average;
+    for (unsigned int i = 0; i < img.height; ++i) {
+        for (unsigned int j = 0; j < img.width; ++j) {
+            img.pixel[i][j].r += img.pixel[i][j].g + img.pixel[i][j].b;
+            img.pixel[i][j].r /= 3;
+            img.pixel[i][j].g = img.pixel[i][j].r;
+            img.pixel[i][j].b = img.pixel[i][j].g;
         }
     }
+    return img;
 }
 
-void sepiaFilter(Image * img) {
+Image sepiaFilter(Image img) {
     // Aplly the sepia filter to the image
-    for (unsigned int x = 0; x < img->height; ++x) {
-        for (unsigned int j = 0; j < img->width; ++j) {
+    for (unsigned int x = 0; x < img.height; ++x) {
+        for (unsigned int j = 0; j < img.width; ++j) {
             Pixel pixel;
-            pixel.r = img->pixel[x][j].r;
-            pixel.g = img->pixel[x][j].g;
-            pixel.b = img->pixel[x][j].b;
+            pixel = img.pixel[x][j];
 
             int p =  pixel.r * .393 + pixel.g * .769 + pixel.b * .189;
-            img->pixel[x][j].r = p;
+            if(p>255) p = 255;
+            img.pixel[x][j].r = p;
 
             p =  pixel.r * .349 + pixel.g * .686 + pixel.b * .168;
-            img->pixel[x][j].g = p;
+            if(p>255) p = 255;
+            img.pixel[x][j].g = p;
 
             p =  pixel.r * .272 + pixel.g * .534 + pixel.b * .131;
-            img->pixel[x][j].b = p;
+            if(p>255) p = 255;
+            img.pixel[x][j].b = p;
         }
     }
+    return img;
 }
     
-int checkMin(int x, int size){
-    if(x - size/2 > 0){
-        return x - size/2;
-    }
-    else{
-        return size/2 - x;
-    }
+int checkMin(int x){
+    if(x >= 0) return x;
+    else return 0;
 }
 
-int checkMax(int x, int size, int max){
-    if(x + size/2 <= max-1){
-        return x+size/2;
-    }
-    else{
-        return max;
-    }
+int checkMax(int x, int y){
+    if(x <= y) return x;
+    else return y;
 }
 
-void blur(Image * img){
+Image blur(Image img){
     // size -> Range of blur
     unsigned int size = 0;
     scanf("%d", &size);
     
     // blur each pixel of the image
-    for (unsigned int i = 0; i < img->height; ++i) {
-        for (unsigned int j = 0; j < img->width; ++j) {
+    for (unsigned int i = 0; i < img.height; ++i) {
+        for (unsigned int j = 0; j < img.width; ++j) {
             Pixel average = {0, 0, 0};
             int nPixels = 0;
             // Getting each pixel that will blur the pixel and 
             // keeping the average on Pixel average
-            unsigned int max_h = checkMax(i,size,img->height);
-            unsigned int max_w = checkMax(j,size,img->width);
-            for(unsigned int x = checkMin(i, size); x < max_h; ++x) {
-                for(unsigned int y = checkMin(j, size); y < max_w; ++y){
-                    average.r += img->pixel[x][y].r;
-                    average.g += img->pixel[x][y].g;
-                    average.b += img->pixel[x][y].b;
+            unsigned int max_h = checkMax(i + size/2,img.height - 1);
+            unsigned int max_w = checkMax(j + size/2,img.width - 1);
+            for(unsigned int x = checkMin(i - size/2); x < max_h; ++x) {
+                for(unsigned int y = checkMin(j - size/2); y < max_w; ++y){
+                    average.r += img.pixel[x][y].r;
+                    average.g += img.pixel[x][y].g;
+                    average.b += img.pixel[x][y].b;
                     nPixels++;
                 }
             }
@@ -126,19 +118,18 @@ void blur(Image * img){
                 average.b /= nPixels;
             }
             // Moving pixel by pixel colors
-            img->pixel[i][j].r = average.r;
-            img->pixel[i][j].g = average.g;
-            img->pixel[i][j].b = average.b;
+            img.pixel[i][j] = average;
         }
     }
+    return img;
 }
 
-void rotate90Right(Image * img) {
+Image rotate90Right(Image img) {
     // Creating a temporary image
-    Image rotate = *img;
+    Image rotate = img;
     // Getting image's sizes
-    rotate.width = img->height;
-    rotate.height = img->width;
+    rotate.width = img.height;
+    rotate.height = img.width;
     // nTimes -> Number of times the image will be rotated
     int nTimes = 0;
     scanf("%d", &nTimes);
@@ -146,21 +137,19 @@ void rotate90Right(Image * img) {
     for (int j = 0; j < nTimes; ++j) {
         for (unsigned int i = 0, y = 0; i < rotate.height; ++i, ++y) {
             for (int j = rotate.width - 1, x = 0; j >= 0; --j, ++x) {
-                rotate.pixel[i][j].r = img->pixel[x][y].r;
-                rotate.pixel[i][j].g = img->pixel[x][y].g;
-                rotate.pixel[i][j].b = img->pixel[x][y].b;
+                rotate.pixel[i][j] = img.pixel[x][y];
             }
         }
     }
-    *img = rotate;
+    return rotate;
 }
 
-void flip(Image * img){
+Image flip(Image img){
     int horizontal;
     scanf("%d", &horizontal);
     // W and h are the width and the height of the image
     // after the flip
-    int w = img->width, h = img->height;
+    int w = img.width, h = img.height;
 
     if (horizontal == 1) w /= 2;
     else h /= 2;
@@ -169,38 +158,34 @@ void flip(Image * img){
         for (int j = 0; j < w; ++j) {
             int x = i, y = j;
 
-            if (horizontal == 1) y = img->width - 1 - j;
-            else x = img->height - 1 - i;
+            if (horizontal == 1) y = img.width - 1 - j;
+            else x = img.height - 1 - i;
 
             Pixel aux1;
-            aux1.r = img->pixel[i][j].r;
-            aux1.g = img->pixel[i][j].g;
-            aux1.b = img->pixel[i][j].b;
+            aux1 = img.pixel[i][j];
 
-            img->pixel[i][j].r = img->pixel[x][y].r;
-            img->pixel[i][j].g = img->pixel[x][y].g;
-            img->pixel[i][j].b = img->pixel[x][y].b;
+            img.pixel[i][j] = img.pixel[x][y];
 
-            img->pixel[x][y].r = aux1.r;
-            img->pixel[x][y].g = aux1.g;
-            img->pixel[x][y].b = aux1.b;
+            img.pixel[x][y] = aux1;
         }
     }
+    return img;
 }
 
-void invertColors(Image * img) {
+Image invertColors(Image img) {
     // Inverts the color of the image by subtracting the maximum color
     // of the actual color of the image
-    for (unsigned int i = 0; i < img->height; ++i) {
-        for (unsigned int j = 0; j < img->width; ++j) {
-            img->pixel[i][j].r = img->nColor - img->pixel[i][j].r;
-            img->pixel[i][j].g = img->nColor - img->pixel[i][j].g;
-            img->pixel[i][j].b = img->nColor - img->pixel[i][j].b;
+    for (unsigned int i = 0; i < img.height; ++i) {
+        for (unsigned int j = 0; j < img.width; ++j) {
+            img.pixel[i][j].r = img.nColor - img.pixel[i][j].r;
+            img.pixel[i][j].g = img.nColor - img.pixel[i][j].g;
+            img.pixel[i][j].b = img.nColor - img.pixel[i][j].b;
         }
     }
+    return img;
 }
 
-void cropImage(Image * img) {
+Image cropImage(Image img) {
     // Coordinates
     // x, y -> beginning of the crop
     // h, w -> size of the image after the crop
@@ -211,30 +196,29 @@ void cropImage(Image * img) {
     // Moving pixels from the cropped part to the beginning of the image
     for(int i = 0; i < h; ++i) {
         for(int j = 0; j < w; ++j) {
-            img->pixel[i][j].r = img->pixel[i + y][j + x].r;
-            img->pixel[i][j].g = img->pixel[i + y][j + x].g;
-            img->pixel[i][j].b = img->pixel[i + y][j + x].b;
+            img.pixel[i][j] = img.pixel[i + y][j + x];
         }
     }
 
     // Sets image's new sizes
-    img->width = w;
-    img->height = h;
+    img.width = w;
+    img.height = h;
+    return img;
 }
 
-void printImage(Image * img){
+void printImage(Image img){
     // Print type of image
-    printf("%s\n", img->type);
+    printf("%s\n", img.type);
 
     // Print width, height and number of colors of the image
-    printf("%u %u\n%d\n", img->width, img->height, img->nColor);
+    printf("%u %u\n%d\n", img.width, img.height, img.nColor);
 
     // Print the image pixel by pixel
-    for (unsigned int i = 0; i < img->height; ++i) {
-        for (unsigned int j = 0; j < img->width; ++j) {
-            printf("%hu %hu %hu ", img->pixel[i][j].r,
-                                   img->pixel[i][j].g,
-                                   img->pixel[i][j].b);
+    for (unsigned int i = 0; i < img.height; ++i) {
+        for (unsigned int j = 0; j < img.width; ++j) {
+            printf("%hu %hu %hu ", img.pixel[i][j].r,
+                                   img.pixel[i][j].g,
+                                   img.pixel[i][j].b);
 
         }
         printf("\n");
@@ -260,31 +244,31 @@ int main() {
 
         switch(option) {
             case 1: { // Grayscale the image
-                grayscale(&img);
+                img = grayscale(img);
                 break;
             }
             case 2: { // Sepia Filter
-                sepiaFilter(&img);
+                img = sepiaFilter(img);
 		        break;
             }
             case 3: { // Blur
-                blur(&img);
+                img = blur(img);
                 break;
             }
             case 4: { // Rotate the imagem 90 degrees to the right
-                rotate90Right(&img);
+                img = rotate90Right(img);
                 break;
             }
             case 5: { // Flip the image 0 = vertical, 1 = horizontal
-                flip(&img);
+                img = flip(img);
                 break;
             }
             case 6: { // Invert the colors
-                invertColors(&img);
+                img = invertColors(img);
                 break;
             }
             case 7: { // Cortar Imagem
-                cropImage(&img);
+                img = cropImage(img);
                 break;
             }
         }
@@ -292,7 +276,7 @@ int main() {
     }
 
     // Print image
-    printImage(&img);
+    printImage(img);
 
     return 0;
 }
